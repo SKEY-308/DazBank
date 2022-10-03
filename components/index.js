93df91ec1f1c2ba2561347dcd86b8cfeb19f3506
Severus KEY <severus.key308@gmail.com>
2022-10-04 00:38:14 +0200

    3th commit

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as images } from './images';
+export { default as styles } from './styles';
+export { default as layout } from './layout';
---
+++ components/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const layout = () => {
+    return (
+        <div>Layout</div>
+    )
+}
+
+export default layout
