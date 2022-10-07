29b85fc27fc9da773a33a76638dd1a46d7ef8eb9
Severus KEY <severus.key308@gmail.com>
2022-10-07 15:31:14 +0200

    19th Com

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -8,11 +8,6 @@ const Testimonials = ({ feed: { icon, image, name, title, content } }) => {
 
     return (
 
-
-
-
-
-
         <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
             <img src={ urlFor(icon) } alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain" />
             <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
@@ -31,10 +26,6 @@ const Testimonials = ({ feed: { icon, image, name, title, content } }) => {
                 </div>
             </div>
         </div>
-
-
-
-
 
     )
 }
